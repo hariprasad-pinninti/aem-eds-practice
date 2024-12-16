6d51275fcb196c1c70dad8da7540b033585512fd
HariPrasadPinninti(T <hari.p@tadigital.com>
2024-12-16 17:39:45 +0530

    design styles

---
+++ blocks/designstyles/designstyles.js
@@ -0,0 +1,21 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const divContainer = document.createElement('div');
+  divContainer.className = 'design-style-container';
+  [...block.children].forEach((row) => {
+    const divContainerItem = document.createElement('div');
+    divContainerItem.className="design-style-item"
+    while (row.firstElementChild) divContainerItem.append(row.firstElementChild);
+    [...divContainerItem.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'design-style-image';
+      else {
+        div.className = 'right-div'
+      }
+    });
+    divContainer.append(divContainerItem);
+  });
+  divContainer.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(divContainer);
+}
